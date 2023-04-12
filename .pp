110f3d364aee1afa463a367f5acd1c1c4d906869
8a0k37 <128235765+8a0k37@users.noreply.github.com>
2023-04-12 21:48:12 +0200

    Update .pp

--- .pp
+++ .pp
@@ -1,22 +1,40 @@
 #!/bin/bash
-length=0
 
+
+source=/.pp_essential/pp_config.conf
+
+startpp="$start"
+endpp="$end"
+ucm=$use_custom_middle
+
+if [[ $use_custom_middle = 1 ]]; then
+  cmiddle="$custom_middle"
+else
+  cmiddle="="
+fi
+
+max_length=$max_pp_length
+max_width=$max_pp_width
+
+length=0
 a_length=""
 
 if [[ "$1" == "-l"* ]]; then
-
-  for (( i=1; i<=$2; i++ ))
-
-  do
-
-    a_length+="="
-
-  done
-  echo "8${a_length}D"
+  if [[ $length -gt $max_length ]]; then
+    echo "The maximum pp length"
+    echo "has been exceeded."
+    echo "(Max: ${max_length}; Your: ${length})"
+    echo "To fix this, please change the maximum"
+    echo "length in the 'pp_config.conf' file."
+    echo "(/.pp_essential/pp_config.conf)
+  else
+    for (( i=1; i<=$2; i++ ))
+    do
+      a_length+="$cmiddle"
+    done
+    echo "${startpp}${a_length}${endpp}"
 else
-
-  echo 'Please select the length using -l (size>
-
-  echo '(one cm = one char)'
-
+  echo "Use pp format: 'pp -l (length)'"
+  echo "(length = number of characters)"
 fi
+exit 1
